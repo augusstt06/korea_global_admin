b02c9c900d8eb7cac55363e5920c6ac958a31d0e
augusstt06 <augusstt06@gmail.com>
2021-10-31 18:53:43 +0900

    add : Recoil in Project

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,16 @@
 import React from "react";
 import '../styles/globals.css';
 import '../styles/Page.modules.scss';
+import {RecoilRoot} from 'recoil';
 
 import Header from '../component/Header';
 
 function MyApp({ Component, pageProps }) {
   return (
-    <div>
+    <RecoilRoot>
       <Header/>
       <Component {...pageProps} />
-    </div>
+    </RecoilRoot>
 
   )
 }
